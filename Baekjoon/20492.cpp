d97d3d1c063e4e7603d6c0859ee22fdd5a0f967b
minchae.lim <mr.comkorea@gmail.com>
2022-04-10 23:24:01 +0900

    first problem

---
+++ Baekjoon/20492.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+#define MAX 800100
+#define INF 1e9
+#define pb(x) push_back(x) 
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<pii> vii;
+typedef vector<int> vi;
+
+int main()
+{
+	int a;
+	scanf("%d", &a);
+	printf("1\n0");
+	return 0;
+}
